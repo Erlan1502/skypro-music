7f4d97248f1a9ee32794d2bd80a77e6af13af59f
Erlan <erlasha07@gmail.com>
2025-06-09 22:09:29 +0500

    hw-1 reworked, добавлены интерфейсы для уменьшение кода и переиспользования - разбит ЦентрБлок для улучшения читаемости

---
+++ src/app/components/TrackListData/TrackListData.tsx
@@ -0,0 +1,40 @@
+export const trackListData = [
+  {
+    id: '1',
+    title: 'Guilt',
+    author: 'Nero',
+    album: 'Welcome Reality',
+    duration: '4:44',
+  },
+  {
+    id: '2',
+    title: 'Elektro',
+    author: 'Dynoro, Outwork, Mr. Gee',
+    album: 'Elektro',
+    duration: '2:22',
+  },
+  {
+    id: '3',
+    title: 'I`m Fire',
+    author: 'Ali Bakgor',
+    album: 'I`m Fire',
+    duration: '2:22',
+    liked: true,
+  },
+  {
+    id: '4',
+    title: 'Non Stop',
+    titleSpan: '(Remix)',
+    author: 'Стоункат, Psychopath',
+    album: 'Non Stop',
+    duration: '4:12',
+  },
+  {
+    id: '5',
+    title: 'Run Run',
+    titleSpan: '(feat. AR/CO)',
+    author: 'Jaded, Will Clarke, AR/CO',
+    album: 'Run Run',
+    duration: '2:54',
+  },
+];
